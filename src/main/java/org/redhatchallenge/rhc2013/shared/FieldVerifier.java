4d6396cb5e0a99d0b0296fefa1f4b3bdd0f629d4
WinnieT <tanxiangfei@gmail.com>
2013-08-30 14:30:56 +0800

    Jun Hao + Gerald + Winnie

---
+++ src/main/java/org/redhatchallenge/rhc2013/shared/FieldVerifier.java
@@ -0,0 +1,49 @@
+package org.redhatchallenge.rhc2013.shared;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: winnie
+ * Date: 8/30/13
+ * Time: 1:51 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class FieldVerifier {
+
+    public static boolean isValidEmail(String email) {
+        return email.toUpperCase().matches("^[0-9A-Z._-]+@[0-9A-Z.]{2,120}$");
+    }
+
+    public static boolean emailIsNull(String email){
+
+        return email.isEmpty();
+    }
+
+    public static boolean passwordIsNull(String password){
+
+        return password.isEmpty();
+    }
+
+    public static boolean fnIsNull(String firstName){
+
+        return firstName.isEmpty();
+    }
+
+    public static boolean lnIsNull(String lastName){
+
+        return lastName.isEmpty();
+    }
+
+    public static boolean contactIsNull(String contact){
+
+        return contact.isEmpty();
+    }
+
+    public static boolean isValidContact(String contact) {
+        return contact.matches("^[0-9]+$");
+    }
+
+    public static boolean schoolIsNull(String school){
+
+        return school.isEmpty();
+    }
+}
